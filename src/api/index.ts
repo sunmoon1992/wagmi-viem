875e47077c916bc826bc62234805ad0424b05715
sunmoon <sunmoon19920211@gmail.com>
2023-10-17 15:45:41 +0800

    fix:

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import { Rec } from '@/typings'
-import { get, post } from '@/utils/http'
+import { get, post } from 'deeblue-apis-test' // deeblue-apis-staging / deeblue-apis
 
 export const x = async (params: Record<string, any>) => {
   const response = await get('x/x', params)
@@ -7,6 +7,6 @@ export const x = async (params: Record<string, any>) => {
 }
 
 export const checkRpcHealthStatus = async (url: string, body: Rec) => {
-  const response = await post(url, body, undefined, true)
+  const response = await post(url, body, undefined)
   return response
 }
