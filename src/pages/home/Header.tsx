2ffd7d8a8b55cffddc54588089e34b6d89d5c043
sunmoon <sunmoon19920211@gmail.com>
2024-01-06 13:00:14 +0800

    fix: xx

--- src/pages/home/Header.tsx
+++ src/pages/home/Header.tsx
@@ -3,16 +3,34 @@ import sogaLogo from '@/assets/soga-logo.svg'
 
 import Connect from '@/components/Modals/Connect'
 import { NavLink } from 'react-router-dom'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { useEffect } from 'react'
+import { clusterApiUrl, Connection, PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js'
 
 const Header = () => {
+  const { publicKey } = useWallet()
+  useEffect(() => {
+    const func = async () => {
+      const programId = new PublicKey(
+        '3XtgZrQb9aQHm2iW8qeTrR1xamKBAbytzqXdtyVgHZB3'
+      )
+      const connection = new Connection("http://localhost:8899", "confirmed")
+
+      const getAccountInfo = await connection.getAccountInfo(publicKey)
+      console.info(getAccountInfo)
+    }
+
+    if (publicKey) void func()
+  }, [publicKey])
+
   return (
-    <div className="soga-header">
-      <div className="logo">
-        <img src={sogaIcon} alt="soga" />
-        <img src={sogaLogo} alt="soga" />
+    <div className='soga-header'>
+      <div className='logo'>
+        <img src={sogaIcon} alt='soga' />
+        <img src={sogaLogo} alt='soga' />
       </div>
       <nav>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to='/'>Home</NavLink>
       </nav>
       <Connect />
     </div>
--- src/pages/home/Part2.tsx
+++ src/pages/home/Part2.tsx
@@ -12,23 +12,23 @@ const Part2 = () => {
             <div>
               <span>Current progress</span>
               <span>
-                <em>21,000</em>
-                <i>/100,000</i>
+                <em>1500</em>
+                <i>/1500</i>
               </span>
             </div>
             <div>
               <span />
-              <span style={{ width: '25%' }} />
-              <em>25%</em>
+              <span style={{ width: '0%' }} />
+              <em>0%</em>
             </div>
             <ul>
               <li>
                 <span>Supply</span>
-                <em>100,000</em>
+                <em>1500</em>
               </li>
               <li>
                 <span>Mint Price</span>
-                <em>0 Sol</em>
+                <em>1 Sol</em>
               </li>
               <li>
                 <span>Balance</span>
