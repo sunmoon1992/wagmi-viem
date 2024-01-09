e5fda9a4dc01631905be3c14998e845b41e68044
sunmoon <sunmoon19920211@gmail.com>
2024-01-09 14:39:48 +0800

    fix: xx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { useScroll2Top } from '@/hooks/useScroll2Top'
 import loadable from '@loadable/component'
 import { BackpackWalletAdapter } from '@solana/wallet-adapter-backpack'
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
-import { clusterApiUrl } from '@solana/web3.js'
-import { useMemo } from 'react'
+import { ConnectionProvider, useWallet, WalletProvider } from '@solana/wallet-adapter-react'
+import { clusterApiUrl, SOLANA_CONNECTION, PublicKey, LAMPORTS_PER_SOL, Connection, Keypair } from '@solana/web3.js'
+import { useEffect, useMemo } from 'react'
 import { isMobile } from 'react-device-detect'
 import { Toaster } from 'react-hot-toast'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import {
+  getAccount, getMint
+} from '@solana/spl-token'
+import { Buffer } from 'buffer'
+import * as web3 from '@solana/web3.js'
+import { struct, u32, ns64 } from '@solana/buffer-layout'
 
 const Home = loadable(() => import('@/pages/home'))
 const HomeH5 = loadable(() => import('@/pages/home/h5'))
@@ -24,8 +30,8 @@ function App() {
       <WalletProvider autoConnect wallets={wallets}>
         <div>
           <Routes>
-            <Route path="/" element={isMobile ? <HomeH5 /> : <Home />} />
-            <Route path="*" element={<Navigate to="/" />} />
+            <Route path='/' element={isMobile ? <HomeH5 /> : <Home />} />
+            <Route path='*' element={<Navigate to='/' />} />
           </Routes>
         </div>
         <Toaster toastOptions={{ style: { background: '#404040', color: '#fff' } }} />
--- src/pages/mint/MintNFT.tsx
+++ src/pages/mint/MintNFT.tsx
@@ -20,16 +20,7 @@ import { Buffer } from 'buffer'
 import { useCallback, useState } from 'react'
 import * as console from 'console'
 import toast from 'react-hot-toast'
-
-const CONTRACT_PROGRAM_ID = new PublicKey('4Vm77PDeH3i9nLhfkAjWaSayTFaGCu6fnW5Jo2qCjg4e')
-const config_info = new PublicKey('13Z64WtxpYvexwSdicwgzj9iT3ustcpSSCF2nVoCF5o1')
-const collection_mint = new PublicKey('3jiHd3gMyK9LjVFFLkNf2LF2Tw8PxANUyuVNX6sps7Wo')
-const collection_metadata = new PublicKey('DEt6GjyEndUzZNUyRrp1AXFhoRfzp1hCnfhhmEw9bawn')
-const collection_edition = new PublicKey('HUUnP1YWWnwqe8oXryvFSXrReBK9ncxp9Dy6QKtQHeLz')
-const collection_authority_record = new PublicKey('GUmS4sjty1b7tNW1zYbbWEDFKxviygspnyqct8TpitbZ')
-const pda_creator = new PublicKey('81JPvLiA5cWgKbwAs1DgufvPTZqfCgeEpmCKSWtPXqoW')
-const charge_address = new PublicKey('9ppiEsLybEazCHHaM1y2AcpEPgHX82aHPZD2E5LuL4ZQ')
-const metadata_program = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s')
+import { minPublicKeyConfig, pda } from '@/utils/callFuncHelpers'
 
 type PublicMintNFTProps = {
   isPublicMint: boolean
@@ -42,32 +33,26 @@ const MintNFT = ({ isPublicMint, isWhiteList, isMinted }: PublicMintNFTProps) =>
   const { publicKey, sendTransaction } = useWallet()
   const [minting, setMinting] = useState(false)
 
-  const recordWhiteListMint = useCallback(async (address: string ,tx: string) => {
-    return  await fetch(
+  const recordWhiteListMint = useCallback(async (address: string, tx: string) => {
+    return await fetch(
       '/api/whiteListMint',
       {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({
           address: address,
-          txid: tx,
-        }),
+          txid: tx
+        })
       }
     ).then((res) => res.json())
   }, [])
 
   const mintNFT = useCallback(async () => {
-    return
     if (!publicKey) {
       return
     }
-    if (!isPublicMint && !isWhiteList && isMinted) {
-      toast.error('Your wallet address is not in the whitelist')
-      return
-    }
-    setMinting(true)
     const lamports = await connection.getBalance(publicKey)
     console.log(
       'Balance:',
@@ -76,12 +61,35 @@ const MintNFT = ({ isPublicMint, isWhiteList, isMinted }: PublicMintNFTProps) =>
         .toString()
     )
 
-    //1、创建一个新的mint 并初始化mint
     const mintTx = new Transaction()
     const mintKey = Keypair.generate()
+    const metadataKey = pda(
+      [
+        Buffer.from('metadata'),
+        minPublicKeyConfig.metadata_program.toBuffer(),
+        mintKey.publicKey.toBuffer()
+      ],
+      minPublicKeyConfig.metadata_program
+    )
+    const edition = pda(
+      [
+        Buffer.from('metadata'),
+        minPublicKeyConfig.metadata_program.toBuffer(),
+        mintKey.publicKey.toBuffer(),
+        Buffer.from('edition')
+      ],
+      minPublicKeyConfig.metadata_program
+    )
+    const user_info = pda(
+      [
+        minPublicKeyConfig.program_id_dev.toBuffer(),
+        publicKey.toBuffer(),
+        Buffer.from('user_info')
+      ],
+      minPublicKeyConfig.program_id_dev
+    )
 
-    mintTx.add(
-      SystemProgram.createAccount({
+    mintTx.add(SystemProgram.createAccount({
         /** The account that will transfer lamports to the created account */
         fromPubkey: publicKey,
         /** Public key of the created account */
@@ -92,62 +100,33 @@ const MintNFT = ({ isPublicMint, isWhiteList, isMinted }: PublicMintNFTProps) =>
         space: MintLayout.span,
         /** Public key of the program to assign as the owner of the created account */
         programId: TOKEN_PROGRAM_ID
-      })
-    )
-
+      }))
     mintTx.add(createInitializeMint2Instruction(mintKey.publicKey, 0, publicKey, publicKey, TOKEN_PROGRAM_ID))
-    //3、通过seed获取 对应钱包下 相应mint的 token——account；
-    const associatedAccount = await getAssociatedTokenAddress(mintKey.publicKey, publicKey)
-
-    mintTx.add(createAssociatedTokenAccountInstruction(publicKey, associatedAccount, publicKey, mintKey.publicKey))
-
-    //2、通过seed['metadata', program id, mint id]生成metadata pda，
-    const metadataKey = PublicKey.findProgramAddressSync(
-      [Buffer.from('metadata'), metadata_program.toBuffer(), mintKey.publicKey.toBuffer()],
-      metadata_program
-    )[0]
-
-    const edition = PublicKey.findProgramAddressSync(
-      [Buffer.from('metadata'), metadata_program.toBuffer(), mintKey.publicKey.toBuffer(), Buffer.from('edition')],
-      metadata_program
-    )[0]
-
-    const user_info = PublicKey.findProgramAddressSync(
-      [CONTRACT_PROGRAM_ID.toBuffer(), publicKey.toBuffer(), Buffer.from('user_info')],
-      CONTRACT_PROGRAM_ID
-    )[0]
-
-    mintTx.add(createMintToInstruction(mintKey.publicKey, associatedAccount, publicKey, 1, [publicKey, mintKey]))
-    // const ata = await getAssociatedTokenAddress(new PublicKey(tokenAddress), wallet.provider.publicKey)
-    // console.log('ata', ata.toBase58())
-    // const account = await getAccount(connection, ata, new PublicKey(tokenAddress))
-    // console.log('account', account)
-    // console.log('account address', account.address.toBase58())
-    // console.log('account amount', account.amount.toString())
-    // console.log('account mint', account.mint.toBase58())
-    // console.log('account owner', account.owner.toBase58())
+    const associatedTokenAddress = await getAssociatedTokenAddress(mintKey.publicKey, publicKey)
+    mintTx.add(createAssociatedTokenAccountInstruction(publicKey, associatedTokenAddress, publicKey, mintKey.publicKey))
+    mintTx.add(createMintToInstruction(mintKey.publicKey, associatedTokenAddress, publicKey, 1, [publicKey, mintKey]))
 
     try {
       const mintInstruction = new TransactionInstruction({
         keys: [
           { pubkey: publicKey, isSigner: true, isWritable: false },
-          { pubkey: config_info, isSigner: false, isWritable: true },
-          { pubkey: pda_creator, isSigner: false, isWritable: true },
+          { pubkey: minPublicKeyConfig.config_info, isSigner: false, isWritable: true },
+          { pubkey: minPublicKeyConfig.pda_creator, isSigner: false, isWritable: true },
           { pubkey: mintKey.publicKey, isSigner: true, isWritable: true },
           { pubkey: metadataKey, isSigner: false, isWritable: true },
           { pubkey: edition, isSigner: false, isWritable: true },
-          { pubkey: collection_mint, isSigner: false, isWritable: false },
-          { pubkey: collection_metadata, isSigner: false, isWritable: true },
-          { pubkey: collection_edition, isSigner: false, isWritable: true },
-          { pubkey: collection_authority_record, isSigner: false, isWritable: true },
-          { pubkey: charge_address, isSigner: false, isWritable: true },
+          { pubkey: minPublicKeyConfig.collection_mint, isSigner: false, isWritable: false },
+          { pubkey: minPublicKeyConfig.collection_metadata, isSigner: false, isWritable: true },
+          { pubkey: minPublicKeyConfig.collection_edition, isSigner: false, isWritable: true },
+          { pubkey: minPublicKeyConfig.collection_authority_record, isSigner: false, isWritable: true },
+          { pubkey: minPublicKeyConfig.charge_address, isSigner: false, isWritable: true },
           { pubkey: user_info, isSigner: false, isWritable: true },
-          { pubkey: metadata_program, isSigner: false, isWritable: false },
+          { pubkey: minPublicKeyConfig.metadata_program, isSigner: false, isWritable: false },
           { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
           { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
           { pubkey: SystemProgram.programId, isSigner: false, isWritable: false }
         ],
-        programId: CONTRACT_PROGRAM_ID,
+        programId: minPublicKeyConfig.program_id_dev,
         data: Buffer.from([2])
       })
       mintTx.add(mintInstruction)
@@ -165,125 +144,8 @@ const MintNFT = ({ isPublicMint, isWhiteList, isMinted }: PublicMintNFTProps) =>
     }
   }, [publicKey, sendTransaction, connection, isWhiteList, isMinted])
 
-  // 定义 WhitelistArgs 的结构和编码方式
-  //   function encodeWhitelistArgs(proof: any) {
-  //     const proofLayout = struct([u8('length'), ...Array(proof.length).fill(BufferLayout.blob(32))])
-  //
-  //     const data = Buffer.alloc(1000) // 分配一个足够大的缓冲区
-  //     const length = proofLayout.encode(
-  //       {
-  //         length: proof.length,
-  //         ...proof
-  //       },
-  //       data
-  //     )
-  //
-  //     return data.slice(0, length)
-  //   }
-
-    // const whitelistMint = useCallback(
-    //   async () => {
-    //     if (!publicKey) {
-    //       alert('Please connect to a wallet')
-    //       return
-    //     }
-    //
-    //     setMinting(true)
-    //
-    //     try {
-    //       // 编码 WhitelistArgs
-    //       const args = encodeWhitelistArgs(isWhiteList)
-    //       const test = [6]
-    //       isWhiteList.forEach((item) => {
-    //        test.push(...item)
-    //       })
-    //       console.log('isWhiteList', test)
-    //       const mintTx = new Transaction()
-    //       const mintKey = Keypair.generate()
-    //
-    //       mintTx.add(
-    //         SystemProgram.createAccount({
-    //           /** The account that will transfer lamports to the created account */
-    //           fromPubkey: publicKey,
-    //           /** Public key of the created account */
-    //           newAccountPubkey: mintKey.publicKey,
-    //           /** Amount of lamports to transfer to the created account */
-    //           lamports: await connection.getMinimumBalanceForRentExemption(MintLayout.span),
-    //           /** Amount of space in bytes to allocate to the created account */
-    //           space: MintLayout.span,
-    //           /** Public key of the program to assign as the owner of the created account */
-    //           programId: TOKEN_PROGRAM_ID
-    //         })
-    //       )
-    //
-    //       mintTx.add(createInitializeMint2Instruction(mintKey.publicKey, 0, publicKey, publicKey, TOKEN_PROGRAM_ID))
-    //       //3、通过seed获取 对应钱包下 相应mint的 token——account；
-    //       const associatedAccount = await getAssociatedTokenAddress(mintKey.publicKey, publicKey)
-    //
-    //       mintTx.add(createAssociatedTokenAccountInstruction(publicKey, associatedAccount, publicKey, mintKey.publicKey))
-    //
-    //       //2、通过seed['metadata', program id, mint id]生成metadata pda，
-    //       const metadataKey = PublicKey.findProgramAddressSync(
-    //         [Buffer.from('metadata'), metadata_program.toBuffer(), mintKey.publicKey.toBuffer()],
-    //         metadata_program
-    //       )[0]
-    //
-    //       const edition = PublicKey.findProgramAddressSync(
-    //         [Buffer.from('metadata'), metadata_program.toBuffer(), mintKey.publicKey.toBuffer(), Buffer.from('edition')],
-    //         metadata_program
-    //       )[0]
-    //
-    //       const user_info = PublicKey.findProgramAddressSync(
-    //         [CONTRACT_PROGRAM_ID.toBuffer(), publicKey.toBuffer(), Buffer.from('user_info')],
-    //         CONTRACT_PROGRAM_ID
-    //       )[0]
-    //
-    //       mintTx.add(createMintToInstruction(mintKey.publicKey, associatedAccount, publicKey, 1, [publicKey, mintKey]))
-    //
-    //       // 创建 WhitelistMint 指令
-    //       const whitelistMintInstruction = new TransactionInstruction({
-    //         keys: [
-    //           { pubkey: publicKey, isSigner: true, isWritable: false },
-    //           { pubkey: config_info, isSigner: false, isWritable: true },
-    //           { pubkey: pda_creator, isSigner: false, isWritable: true },
-    //           { pubkey: mintKey.publicKey, isSigner: true, isWritable: true },
-    //           { pubkey: metadataKey, isSigner: false, isWritable: true },
-    //           { pubkey: edition, isSigner: false, isWritable: true },
-    //           { pubkey: collection_mint, isSigner: false, isWritable: false },
-    //           { pubkey: collection_metadata, isSigner: false, isWritable: true },
-    //           { pubkey: collection_edition, isSigner: false, isWritable: true },
-    //           { pubkey: collection_authority_record, isSigner: false, isWritable: true },
-    //           { pubkey: charge_address, isSigner: false, isWritable: true },
-    //           { pubkey: user_info, isSigner: false, isWritable: true },
-    //           { pubkey: metadata_program, isSigner: false, isWritable: false },
-    //           { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
-    //           { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
-    //           { pubkey: SystemProgram.programId, isSigner: false, isWritable: false }
-    //         ],
-    //         programId: CONTRACT_PROGRAM_ID,
-    //         data: Buffer.concat([Buffer.from([3]), args]) // WhitelistMint 指令下标和参数
-    //       })
-    //       // 创建并发送交易
-    //       // const transaction = new Transaction().add(whitelistMintInstruction)
-    //       mintTx.add(whitelistMintInstruction)
-    //       const signature = await sendTransaction(mintTx, connection, { signers: [mintKey] })
-    //       console.log(signature)
-    //       await connection.confirmTransaction(signature, 'confirmed')
-    //       // const signature = await sendTransaction(transaction, connection)
-    //       // await connection.confirmTransaction(signature, 'confirmed')
-    //       alert('Whitelist NFT Minted Successfully!')
-    //     } catch (error) {
-    //       console.error(error)
-    //       alert('Minting Failed!')
-    //     } finally {
-    //       setMinting(false)
-    //     }
-    //   },
-    //   [publicKey, sendTransaction, connection, isWhiteList]
-    // )
-
   return (
-    <button onClick={mintNFT} disabled={(!publicKey || minting) || (!isPublicMint && !isWhiteList) || isMinted}>
+    <button onClick={mintNFT}>
       {minting ? 'Minting...' : 'Go To MINT'}
     </button>
   )
